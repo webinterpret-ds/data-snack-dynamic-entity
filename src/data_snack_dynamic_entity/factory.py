2d056a9eda3576a1eface9468e9281cf3f25046f
Wojciech Włodarczyk <heolin123@gmail.com>
2023-04-06 14:26:32 +0200

    Add support for complex types

---
+++ src/data_snack_dynamic_entity/factory.py
@@ -0,0 +1,78 @@
+from data_snack.entities import Entity
+from dataclasses import dataclass, field, make_dataclass
+from typing import Dict, Type, Any, Type, List, Optional
+
+from data_snack_dynamic_entity.types import EntitySchema, FieldSchema, EntityTemplates
+from data_snack_dynamic_entity.validate import validate_entity_templates
+
+
+def _create_entity_template(entity_name: str, keys: List[str], excluded_fields: List[str]) -> Type:
+    return type(
+        f"{entity_name}Template",
+        (Entity, ),
+        {"Meta": type("Meta", (object, ), {"keys": keys, "excluded_fields": excluded_fields})}
+    )
+
+
+@dataclass
+class EntityFactory:
+    types_mapping: Dict[str, Any] = field(default_factory=dict)  # this will store custom mappings
+
+    def load_entities(self, templates: EntityTemplates) -> Dict[str, Type]:
+        """
+        Creates new Entity types based on provided config.
+        Config should contain a valid template that is created according to the schema (see README).
+
+        :param templates: input config containing templates
+        :returns: a dictionary with all created Entity types
+        """
+
+        validate_entity_templates(templates)
+        return {
+            entity_name: self._create_entity(entity_name, entity_schema)
+            for entity_name, entity_schema in templates.items()
+        }
+
+    def _create_entity(self, entity_name: str, entity_schema: EntitySchema) -> Type:
+        """
+        Creates entity of given name according to given schema.
+        :param entity_name: new entity name
+        :param entity_schema: new entity schema
+        :return: new entity type
+        """
+        fields = []
+        fields_with_defaults = []
+        keys = []
+        excluded_fields = []
+        for field_name, field_schema in entity_schema["properties"].items():
+            field_type = self._get_entity_type(field_schema["type"])
+
+            if field_schema.get("key"):
+                keys.append(field_name)
+            if field_schema.get("excluded"):
+                excluded_fields.append(field_name)
+            if field_schema.get("optional"):
+                field_type = Optional[field_type]
+            if "default" in field_schema:
+                fields_with_defaults.append((field_name, field_type, field(default=field_schema["default"])))
+            else:
+                fields.append((field_name, field_type))
+
+        entity_template = _create_entity_template(entity_name=entity_name, keys=keys, excluded_fields=excluded_fields)
+        return make_dataclass(
+            entity_name, fields + fields_with_defaults, bases=(entity_template,), namespace={"__module__": __name__}
+        )
+
+    def _get_entity_type(self, name: str) -> Type:
+        t = __builtins__.get(name)
+        if isinstance(t, type):
+            return t
+        elif t := self.types_mapping.get(name):
+            return t
+        raise ValueError(name)
+
+
+def load_entities(templates: EntityTemplates, types_mapping: Dict[str, Any]=None) -> Dict[str, Type]:
+    if not types_mapping:
+        types_mapping = {}
+    return EntityFactory(types_mapping).load_entities(templates)
--- tests/data_snack_dynamic_entity/test_factory.py
+++ tests/data_snack_dynamic_entity/test_factory.py
@@ -1,9 +1,25 @@
+import numpy as np
+import pytest
+from unittest.mock import MagicMock
 from data_snack.entities import Entity
+from typing import Dict, Any, Type, Union, get_origin, get_args, get_type_hints
+from unittest.mock import patch
 
 from data_snack_dynamic_entity import load_entities
-from typing import Type, Union, get_origin, get_args, get_type_hints
-
+from data_snack_dynamic_entity.factory import EntityFactory
 from data_snack_dynamic_entity.types import EntityTemplates
+
+
+@pytest.fixture
+def entity_factory() -> EntityFactory:
+    return EntityFactory()
+
+
+@pytest.fixture
+def entity_factory_with_mapping() -> EntityFactory:
+    return EntityFactory({
+        "numpy.float16": np.float16
+    })
 
 
 def _is_optional(field_type: Type) -> bool:
@@ -16,9 +32,11 @@ def _is_optional(field_type: Type) -> bool:
     return get_origin(field_type) is Union and type(None) in get_args(field_type)
 
 
-def test_load_entities(entity_templates: EntityTemplates) -> None:
-    """Testing if `load_entities` is correctly parsing template and creating a new type."""
-    entities = load_entities(entity_templates)
+def test__entity_factory__load_entities(
+        entity_factory: EntityFactory, entity_templates: EntityTemplates
+) -> None:
+    """Testing if `EntityFactory.load_entities` is correctly parsing template and creating a new type."""
+    entities = entity_factory.load_entities(entity_templates)
 
     assert len(entities) == 1
 
@@ -39,9 +57,11 @@ def test_load_entities(entity_templates: EntityTemplates) -> None:
     assert car.cost == 10.0
 
 
-def test_load_entities_many(entity_templates_many: EntityTemplates) -> None:
+def test_entity_factory__load_entities_many(
+        entity_factory: EntityFactory, entity_templates_many: EntityTemplates
+) -> None:
     """Testing if `load_entities` works correctly if more than one Entity is defined in the template."""
-    entities = load_entities(entity_templates_many)
+    entities = entity_factory.load_entities(entity_templates_many)
 
     assert len(entities) == 2
     for entity in entities.values():
@@ -51,7 +71,9 @@ def test_load_entities_many(entity_templates_many: EntityTemplates) -> None:
         assert isinstance(obj, Entity)
 
 
-def test_load_entities_default_values() -> None:
+def test__entity_factory__load_entities_default_values(
+        entity_factory: EntityFactory
+) -> None:
     """
     Testing if default values of fields are assigned as expected (key = False, optional = False, excluded = False).
     """
@@ -63,7 +85,7 @@ def test_load_entities_default_values() -> None:
             }
         }
     }
-    entities = load_entities(schema)
+    entities = entity_factory.load_entities(schema)
     Car = entities["Car"]
     assert get_type_hints(Car)["tested_field"] is int
     assert Car.get_keys() == ["index"]
@@ -71,7 +93,7 @@ def test_load_entities_default_values() -> None:
     assert Car.get_fields() == ["index", "tested_field"]
 
 
-def test_load_entities_fields_ordering() -> None:
+def test__entity_factory__load_entities_fields_ordering(entity_factory: EntityFactory) -> None:
     """
     Testing if fields are properly ordered during creation - the default ones should be at the end due to python syntax
     limitations. Scenario simplifies to successful entity creation even if fields with defaults are defined before
@@ -86,12 +108,12 @@ def test_load_entities_fields_ordering() -> None:
             }
         }
     }
-    entities = load_entities(schema)
+    entities = entity_factory.load_entities(schema)
     Car = entities["Car"]
     assert Car
 
 
-def test_load_entities_excluded_key() -> None:
+def test__entity_factory__load_entities_excluded_key(entity_factory) -> None:
     """
     Testing if key can be excluded
     """
@@ -103,8 +125,47 @@ def test_load_entities_excluded_key() -> None:
             }
         }
     }
-    entities = load_entities(schema)
+    entities = entity_factory.load_entities(schema)
     Car = entities["Car"]
     assert Car.get_keys() == ["index"]
     assert Car.get_excluded_fields() == ["index"]
     assert Car.get_fields() == ["other_field"]
+
+
+def test__entity_factory__load_entities_with_types_mappings(entity_factory_with_mapping: EntityFactory) -> None:
+    """
+    Testing if key can be excluded
+    """
+    schema = {
+        "Car": {
+            "properties": {
+                "index": {"type": "int", "key": True, "excluded": True},
+                "numpy_field": {"type": "numpy.float16"}
+            }
+        }
+    }
+    entities = entity_factory_with_mapping.load_entities(schema)
+    Car = entities["Car"]
+    assert Car.get_keys() == ["index"]
+    assert Car.get_excluded_fields() == ["index"]
+    assert Car.get_fields() == ["numpy_field"]
+    assert get_type_hints(Car)["numpy_field"] is np.float16
+
+
+@patch('data_snack_dynamic_entity.factory.EntityFactory')
+def test__load_entities__passing_types_mapping(
+        entity_factory_mock: MagicMock,
+        entity_templates: EntityTemplates
+):
+    types_mapping = {"numpy.float16": np.float16}
+    load_entities(entity_templates, types_mapping)
+    entity_factory_mock.assert_called_with(types_mapping)
+
+
+@patch('data_snack_dynamic_entity.factory.EntityFactory.load_entities')
+def test__load_entities__executing_load_entities(
+        load_entities_mock: MagicMock,
+        entity_templates: EntityTemplates
+):
+    load_entities(entity_templates, {})
+    load_entities_mock.assert_called_with(entity_templates)
